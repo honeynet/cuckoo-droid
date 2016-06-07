512bf6a98b6852f540c89dba9c4053481546f7a5
Tin Duong <tinduong@vnsecurity.net>
2016-06-07 19:09:00 +0700

    add apis for droidbot
    add more apis to support droidbot

--- analyzer/android/lib/api/adb.py
+++ analyzer/android/lib/api/adb.py
@@ -6,7 +6,7 @@
 import logging
 import os
 import subprocess
-
+import re
 from lib.common.utils import send_file
 
 log = logging.getLogger(__name__)
@@ -86,3 +86,152 @@ def take_screenshot(filename):
         return
 
     return "/sdcard/%s" % filename
+
+def shell(command):
+    """
+    Execute a command
+    """
+    output = ""
+    if isinstance(command, list):
+        args = command
+    elif isinstance(command, str):
+        args = command.split(" ")
+    else:
+        log.error("Error executing command with type: ", type(command))
+        return output
+    try:
+        output = subprocess.check_output(args)
+    except subprocess.CalledProcessError as e:
+        log.error("Error executing command: %r", e)
+    except OSError as e:
+        log.error("Error executing command: %r", e)
+    return output
+
+
+def getPackagePath(package_name):
+    """
+    Get installed path of a package
+    """
+    command = "pm path %s" % package_name
+    path = shell(command)
+    if path:
+        path = path.split(":")[1]
+    return path
+
+
+#Modified APIs from AndroidViewClient
+#https://github.com/dtmilano/AndroidViewClient/
+def getTopActivityName():
+    """
+    Get current activity
+    """
+    data = shell("dumpsys activity top").splitlines()
+    regex = re.compile("\s*ACTIVITY ([A-Za-z0-9_.]+)/([A-Za-z0-9_.]+)")
+    m = regex.search(data)
+    if m:
+        return m.group(1) + "/" + m.group(2)
+
+def getDisplayInfo():
+    displayInfo = getLogicalDisplayInfo()
+    if displayInfo:
+        return displayInfo
+    displayInfo = getPhysicalDisplayInfo()
+    if displayInfo:
+        return displayInfo
+    log.error("Error getting display info")
+    return None
+
+def getLogicalDisplayInfo():
+    """
+    Gets C{mDefaultViewport} and then C{deviceWidth} and C{deviceHeight} values from dumpsys.
+    This is a method to obtain display logical dimensions and density
+    """
+    logicalDisplayRE = re.compile(".*DisplayViewport{valid=true, .*orientation=(?P<orientation>\d+), .*deviceWidth=(?P<width>\d+), deviceHeight=(?P<height>\d+).*")
+    for line in shell("dumpsys display").splitlines():
+        m = logicalDisplayRE.search(line, 0)
+        if m:
+            displayInfo = {}
+            for prop in ["width", "height", "orientation"]:
+                displayInfo[prop] = int(m.group(prop))
+            for prop in ["density"]:
+                d = getDisplayDensity(None, strip=True, invokeGetPhysicalDisplayIfNotFound=True)
+                if d:
+                    displayInfo[prop] = d
+                else:
+                    # No available density information
+                    displayInfo[prop] = -1.0
+            return displayInfo
+    return None
+
+def getPhysicalDisplayInfo():
+    """
+    Gets C{mPhysicalDisplayInfo} values from dumpsys. This is a method to obtain display dimensions and density
+    """
+    phyDispRE = re.compile("Physical size: (?P<width>)x(?P<height>).*Physical density: (?P<density>)", re.MULTILINE)
+    data = shell("wm size") + shell("wm density")
+    m = phyDispRE.search(data)
+    if m:
+        displayInfo = {}
+        for prop in ["width", "height"]:
+            displayInfo[prop] = int(m.group(prop))
+        for prop in ["density"]:
+            displayInfo[prop] = float(m.group(prop))
+        return displayInfo
+    phyDispRE = re.compile(
+        ".*PhysicalDisplayInfo{(?P<width>\d+) x (?P<height>\d+), .*, density (?P<density>[\d.]+).*")
+    for line in shell("dumpsys display").splitlines():
+        m = phyDispRE.search(line, 0)
+        if m:
+            displayInfo = {}
+            for prop in ["width", "height"]:
+                displayInfo[prop] = int(m.group(prop))
+            for prop in ["density"]:
+                # In mPhysicalDisplayInfo density is already a factor, no need to calculate
+                displayInfo[prop] = float(m.group(prop))
+            return displayInfo
+    # This could also be mSystem or mOverscanScreen
+    phyDispRE = re.compile("\s*mUnrestrictedScreen=\((?P<x>\d+),(?P<y>\d+)\) (?P<width>\d+)x(?P<height>\d+)")
+    # This is known to work on older versions (i.e. API 10) where mrestrictedScreen is not available
+    dispWHRE = re.compile("\s*DisplayWidth=(?P<width>\d+) *DisplayHeight=(?P<height>\d+)")
+    for line in shell("dumpsys window").splitlines():
+        m = phyDispRE.search(line, 0)
+        if not m:
+            m = dispWHRE.search(line, 0)
+        if m:
+            displayInfo = {}
+            BASE_DPI = 160.0
+            for prop in ["width", "height"]:
+                displayInfo[prop] = int(m.group(prop))
+            for prop in ["density"]:
+                d = 0
+                if displayInfo and "density" in displayInfo:
+                    d = displayInfo["density"]
+                else:
+                    _d = shell("getprop ro.sf.lcd_density").strip()
+                    if _d:
+                        d = float(_d) / BASE_DPI
+                    else:
+                        _d = shell("getprop qemu.sf.lcd_density").strip()
+                        if _d:
+                            d = float(_d) / BASE_DPI
+                if d:
+                    displayInfo[prop] = d
+                else:
+                    # No available density information
+                    displayInfo[prop] = -1.0
+            return displayInfo
+    return None
+
+def unlock():
+    """
+    Unlock the screen of the device
+    """
+    shell("input keyevent MENU")
+    shell("input keyevent BACK")
+
+def press(key):
+    """
+    Press a key
+    """
+    shell("input keyevent %s" % key)
+    
