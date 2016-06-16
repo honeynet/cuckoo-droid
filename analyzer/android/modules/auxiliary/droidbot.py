f0ceabd6ecac8d2c8cac197b6533e25834940abe
Tin Duong <tinduong@vnsecurity.net>
2016-06-16 13:51:20 +0700

    update droidbot module

--- analyzer/android/modules/auxiliary/droidbot.py
+++ analyzer/android/modules/auxiliary/droidbot.py
@@ -5,17 +5,17 @@
 
 import logging
 import sys
+import time
 import os
+from lib.api import adb
 from threading import Thread
-from lib.api.droidbot_types import App, Device
-from lib.api.droidbot_env import AppEnvManager
-from lib.api.droidbot_event import AppEventManager
-from lib.api.adb import getLastInstalledPackagePath
+from lib.api.droidbot.dtypes import App, Device
+from lib.api.droidbot.event import AppEventManager
 from lib.common.abstracts import Auxiliary
 
 log = logging.getLogger(__name__)
 
-class DroidBot(Auxiliary):
+class DroidBot(Auxiliary, Thread):
 	"""
 	DroidBot module
 	A robot which interact with Android automatically
@@ -24,33 +24,38 @@ class DroidBot(Auxiliary):
 		"""
 		initiate droidbot instance
 		"""
+		Thread.__init__(self)
 		self.output_dir = os.path.abspath("droidbot_out")
 		if not os.path.exists(self.output_dir):
 			os.mkdir(self.output_dir)
 
 		self.device = Device(self.output_dir)
-		self.app_path = getLastInstalledPackagePath()
+		self.app_path = adb.getLastInstalledPackagePath()
 		self.app = App(self.app_path, self.output_dir)
 
 		#Since we use the default environment, no need to deploy env_manager
 		#self.env_manager = AppEnvManager(self.app)
 		self.event_manager = AppEventManager(device = self.device, app = self.app)
 
-	"""
+	
 	def run(self):
 		log.info("Starting DroidBot")
+		while not self.device.is_foreground(self.app):
+			log.info("Waiting for app to be executed")
+			time.sleep(2)
+
 		try:
-			#self.env_manager.deploy()
 			self.event_manager.start()
-		except:
+		except KeyboardInterrupt:
 			pass
 
-		self.stop()
-		log.info("DroidBot stopped")
+		return True
 
 	def stop(self):
-		#self.env_manager.stop()
+		self.device.disconnect()
 		self.event_manager.stop()
+		log.info("Droidbot stopped")
+		return
 
 
-	"""
+	
